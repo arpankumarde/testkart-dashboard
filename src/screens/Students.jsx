5c0764a7623f270ef2baf781922a06a7d3c2d76b
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2024-01-24 23:38:34 +0530

    Binded Students and Student Details Api

--- src/screens/Students.jsx
+++ src/screens/Students.jsx
@@ -1,103 +1,31 @@
-import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks";
+import { server } from "../api";
 import { PiStudentFill } from "react-icons/pi";
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { BiLoaderAlt } from "react-icons/bi";
 
 const Students = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
 
-  const studentsData = [
-    {
-      studentId: "STD00001",
-      name: "John Doe",
-      testSeriesBought: 2,
-      amountSpent: 150,
-    },
-    {
-      studentId: "STD00002",
-      name: "Jane Smith",
-      testSeriesBought: 1,
-      amountSpent: 75,
-    },
-    {
-      studentId: "STD00003",
-      name: "Alice Johnson",
-      testSeriesBought: 3,
-      amountSpent: 200,
-    },
-    {
-      studentId: "STD00004",
-      name: "Bob Williams",
-      testSeriesBought: 0,
-      amountSpent: 0,
-    },
-    {
-      studentId: "STD00005",
-      name: "Emily Davis",
-      testSeriesBought: 5,
-      amountSpent: 400,
-    },
-    {
-      studentId: "STD00006",
-      name: "Michael Brown",
-      testSeriesBought: 2,
-      amountSpent: 150,
-    },
-    {
-      studentId: "STD00007",
-      name: "Olivia Wilson",
-      testSeriesBought: 4,
-      amountSpent: 300,
-    },
-    {
-      studentId: "STD00008",
-      name: "James Taylor",
-      testSeriesBought: 1,
-      amountSpent: 75,
-    },
-    {
-      studentId: "STD00009",
-      name: "Sophia Anderson",
-      testSeriesBought: 3,
-      amountSpent: 200,
-    },
-    {
-      studentId: "STD00010",
-      name: "Benjamin Martinez",
-      testSeriesBought: 2,
-      amountSpent: 150,
-    },
-    {
-      studentId: "STD00011",
-      name: "Ava Hernandez",
-      testSeriesBought: 1,
-      amountSpent: 75,
-    },
-    {
-      studentId: "STD00012",
-      name: "William Thompson",
-      testSeriesBought: 4,
-      amountSpent: 300,
-    },
-    {
-      studentId: "STD00013",
-      name: "Mia Garcia",
-      testSeriesBought: 2,
-      amountSpent: 150,
-    },
-    {
-      studentId: "STD00014",
-      name: "Liam Robinson",
-      testSeriesBought: 3,
-      amountSpent: 200,
-    },
-  ];
+  const [studentReport, setStudentReport] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // If user is not logged in, redirect to login page
     if (!user) return navigate("/login");
+
+    server
+      .get("/api/v1/studio/academy/student-report")
+      .then((res) => {
+        setStudentReport(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }, [user, navigate]);
 
   return (
@@ -105,16 +33,17 @@ const Students = () => {
       <div className="flex flex-col md:flex-row md:gap-4 lg:gap-8">
         <div className="bg-white md:rounded-md p-4 flex-1">
           <h2 className="text-xl flex items-center gap-2">
-            <i>
+            <i className="p-2">
               <PiStudentFill size={25} />
             </i>
             <span>Students</span>
           </h2>
           <hr className="my-4" />
-          <div className="min-h-96 h-full overflow-auto">
+          <div className="h-[calc(100dvh-10rem-0.6rem)] lg:h-[calc(100dvh-14rem-0.6rem)] overflow-auto">
             <table className="w-full">
-              <thead className="sticky top-0 bg-white">
+              <thead className="sticky top-0 bg-white border-b">
                 <tr className="text-center">
+                  <th className="px-4 py-2">#</th>
                   <th className="px-4 py-2">Student ID</th>
                   <th className="px-4 py-2">Name</th>
                   <th className="px-4 py-2">Test Series Bought</th>
@@ -122,22 +51,56 @@ const Students = () => {
                 </tr>
               </thead>
               <tbody className="text-gray-700 hover:[&>*]:text-gray-950 hover:[&>*]:bg-gray-100">
-                {studentsData.map((student, index) => (
-                  <tr className="text-center" key={index}>
-                    <td className="px-4 py-2 text-blue-600 hover:underline">
-                      <Link to={`/students/${student.studentId}`}>
-                        {student.studentId}
-                      </Link>
+                {studentReport.length != 0 ? (
+                  studentReport.map((student, index) => (
+                    <tr className="text-center border-b" key={index}>
+                      <td className="px-4 py-2">{index + 1}</td>
+                      <td
+                        className="px-4 py-2 text-blue-600 hover:text-blue-700 hover:underline cursor-pointer"
+                        onClick={() =>
+                          navigate(`/students/${student.student_id}`)
+                        }
+                      >
+                        {student.student_id ? student.student_id : "NA"}
+                      </td>
+                      <td
+                        className="px-4 py-2 text-blue-600 hover:text-blue-700 hover:underline cursor-pointer"
+                        onClick={() =>
+                          navigate(`/students/${student.student_id}`)
+                        }
+                      >
+                        {student.student_name
+                          ? student.student_name
+                          : "Untitled"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {student.test_series_purchased
+                          ? student.test_series_purchased
+                          : 0}
+                      </td>
+                      <td className="px-4 py-2">
+                        {student.total_price ? student.total_price : 0}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr className="text-center">
+                    <td colSpan={5} className="px-4 py-2">
+                      {loading ? (
+                        <>
+                          <span>Loading Students</span>
+                          {"  "}
+                          <BiLoaderAlt
+                            className="inline animate-spin"
+                            size={20}
+                          />
+                        </>
+                      ) : (
+                        "No Students found"
+                      )}
                     </td>
-                    <td className="px-4 py-2 text-blue-600 hover:underline">
-                      <Link to={`/students/${student.studentId}`}>
-                        {student.name}
-                      </Link>
-                    </td>
-                    <td className="px-4 py-2">{student.testSeriesBought}</td>
-                    <td className="px-4 py-2">{student.amountSpent}</td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
           </div>
--- src/screens/StudentsDetails.jsx
+++ src/screens/StudentsDetails.jsx
@@ -1,105 +1,33 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../hooks";
+import { server } from "../api";
 import { IoChevronBackOutline } from "react-icons/io5";
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { BiLoaderAlt } from "react-icons/bi";
 
 const studentData = () => {
   const { user } = useAuth();
   const { stdid } = useParams();
   const navigate = useNavigate();
 
-  const studentData = [
-    {
-      seriesName: "Dummy Test 1",
-      status: "free",
-      amount: 0,
-      datetime: "2022-01-01T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 2",
-      status: "paid",
-      amount: 10,
-      datetime: "2022-01-02T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 3",
-      status: "free",
-      amount: 0,
-      datetime: "2022-01-03T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 4",
-      status: "paid",
-      amount: 15,
-      datetime: "2022-01-04T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 5",
-      status: "free",
-      amount: 0,
-      datetime: "2022-01-05T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 6",
-      status: "paid",
-      amount: 20,
-      datetime: "2022-01-06T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 7",
-      status: "free",
-      amount: 0,
-      datetime: "2022-01-07T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 8",
-      status: "paid",
-      amount: 25,
-      datetime: "2022-01-08T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 9",
-      status: "free",
-      amount: 0,
-      datetime: "2022-01-09T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 10",
-      status: "paid",
-      amount: 30,
-      datetime: "2022-01-10T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 11",
-      status: "free",
-      amount: 0,
-      datetime: "2022-01-11T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 12",
-      status: "paid",
-      amount: 35,
-      datetime: "2022-01-12T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 13",
-      status: "free",
-      amount: 0,
-      datetime: "2022-01-13T10:00:00Z",
-    },
-    {
-      seriesName: "Dummy Test 14",
-      status: "paid",
-      amount: 40,
-      datetime: "2022-01-14T10:00:00Z",
-    },
-  ];
+  const [studentTestData, setStudentTestData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // If user is not logged in, redirect to login page
     if (!user) return navigate("/login");
-  }, [user, navigate]);
+
+    server
+      .get(`/api/v1/studio/academy/student-report/${stdid}`)
+      .then((res) => {
+        setStudentTestData(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, [user, navigate, stdid]);
 
   return (
     <section className="md:p-4 lg:p-8">
@@ -116,35 +44,74 @@ const studentData = () => {
             </Link>
             <span>Student Details</span>
           </h2>
-          <hr className="my-4"/>
-          <div className="min-h-96 h-full overflow-auto">
+          <hr className="my-4" />
+          <div className="h-[calc(100dvh-10rem-0.6rem)] lg:h-[calc(100dvh-14rem-0.6rem)] overflow-auto">
             <table className="w-full">
-              <thead className="sticky top-0 bg-white">
+              <thead className="sticky top-0 bg-white border-b">
                 <tr className="text-center">
                   <th className="px-4 py-2">#</th>
-                  <th className="px-4 py-2">Test Series</th>
+                  <th className="px-4 py-2">
+                    <span className="hidden lg:inline">Test</span>
+                    {"  "}Series ID
+                  </th>
+                  <th className="px-4 py-2">
+                    <span className="hidden lg:inline">Test</span>
+                    {"  "}Series Name
+                  </th>
                   <th className="px-4 py-2">Status</th>
                   <th className="px-4 py-2">Amount</th>
-                  <th className="px-4 py-2">Date & Time</th>
+                  <th className="px-4 py-2">Purchase Date</th>
                 </tr>
               </thead>
               <tbody
                 className="text-gray-700 hover:[&>*]:text-gray-950 hover:[&>*]:bg-gray-100"
                 onClick={() => setSecondaryTable((prev) => !prev)}
               >
-                {studentData.map((test, index) => (
-                  <tr className="text-center capitalize" key={index}>
-                    <td className="px-4 py-2">{index + 1}</td>
-                    <td className="px-4 py-2">{test.seriesName}</td>
-                    <td className="px-4 py-2">{test.status}</td>
-                    <td className="px-4 py-2">
-                      {test.amount == "0" ? "-" : test.amount}
-                    </td>
-                    <td className="px-4 py-2">
-                      {new Date(test.datetime).toLocaleString()}
+                {studentTestData.length != 0 ? (
+                  studentTestData.map((test, index) => (
+                    <tr className="text-center capitalize border-b" key={index}>
+                      <td className="px-4 py-2">{index + 1}</td>
+                      <td className="px-4 py-2">
+                        {test.test_series_id ? test.test_series_id : "NA"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.test_sery.title
+                          ? test.test_sery.title
+                          : "Untitled"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.is_paid == 0 ? "Free" : "Paid"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.test_sery.is_paid == 0
+                          ? test.test_sery.price
+                          : "-"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.purchase_time
+                          ? new Date(test.purchase_time).toLocaleString()
+                          : "NA"}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr className="text-center">
+                    <td colSpan={6} className="px-4 py-2">
+                      {loading ? (
+                        <>
+                          <span>Loading Test Series</span>
+                          {"  "}
+                          <BiLoaderAlt
+                            className="inline animate-spin"
+                            size={20}
+                          />
+                        </>
+                      ) : (
+                        "No Test Series found"
+                      )}
                     </td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
           </div>
