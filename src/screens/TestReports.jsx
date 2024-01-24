cbc256c6152621619cbd4801076304e91d586c69
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2024-01-24 23:39:34 +0530

    Binded Test Report and Test Report Details Api

--- src/screens/TestReports.jsx
+++ src/screens/TestReports.jsx
@@ -1,33 +1,31 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks";
+import { server } from "../api";
 import { HiOutlineDocumentReport } from "react-icons/hi";
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { BiLoaderAlt } from "react-icons/bi";
 
 const TestReports = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
 
-  const testSeriesData = [
-    { testSeriesName: "Dummy Test Series 1", studentsJoined: 32 },
-    { testSeriesName: "Dummy Test Series 2", studentsJoined: 25 },
-    { testSeriesName: "Dummy Test Series 3", studentsJoined: 40 },
-    { testSeriesName: "Dummy Test Series 4", studentsJoined: 28 },
-    { testSeriesName: "Dummy Test Series 5", studentsJoined: 35 },
-    { testSeriesName: "Dummy Test Series 6", studentsJoined: 30 },
-    { testSeriesName: "Dummy Test Series 7", studentsJoined: 38 },
-    { testSeriesName: "Dummy Test Series 8", studentsJoined: 27 },
-    { testSeriesName: "Dummy Test Series 9", studentsJoined: 33 },
-    { testSeriesName: "Dummy Test Series 10", studentsJoined: 26 },
-    { testSeriesName: "Dummy Test Series 11", studentsJoined: 42 },
-    { testSeriesName: "Dummy Test Series 12", studentsJoined: 29 },
-    { testSeriesName: "Dummy Test Series 13", studentsJoined: 36 },
-    { testSeriesName: "Dummy Test Series 14", studentsJoined: 31 },
-  ];
+  const [testSeriesReport, setTestSeriesReport] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // If user is not logged in, redirect to login page
     if (!user) return navigate("/login");
+
+    server
+      .get("/api/v1/studio/academy/series-report/")
+      .then((res) => {
+        setTestSeriesReport(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }, [user, navigate]);
 
   return (
@@ -35,33 +33,72 @@ const TestReports = () => {
       <div className="flex flex-col md:flex-row md:gap-4 lg:gap-8">
         <div className="bg-white md:rounded-md p-4 flex-1">
           <h2 className="text-xl flex items-center gap-2">
-            <i>
+            <i className="p-2">
               <HiOutlineDocumentReport size={25} />
             </i>
             <span>Test Reports</span>
           </h2>
           <hr className="my-4" />
-          <div className="min-h-96 h-full overflow-auto">
+          <div className="h-[calc(100dvh-10rem-0.6rem)] lg:h-[calc(100dvh-14rem-0.6rem)] overflow-auto">
             <table className="w-full">
-              <thead className="sticky top-0 bg-white">
+              <thead className="sticky top-0 bg-white border-b">
                 <tr className="text-center">
                   <th className="px-4 py-2">#</th>
-                  <th className="px-4 py-2">Test Series</th>
-                  <th className="px-4 py-2">No. of Students Joined</th>
+                  <th className="px-4 py-2">
+                    <span className="hidden lg:inline">Test</span>
+                    {"  "}Series ID
+                  </th>
+                  <th className="px-4 py-2 min-w-40">
+                    <span className="hidden lg:inline">Test</span>
+                    {"  "}Series Name
+                  </th>
+                  <th className="px-4 py-2">Students Joined</th>
                 </tr>
               </thead>
               <tbody className="text-gray-700 hover:[&>*]:text-gray-950 hover:[&>*]:bg-gray-100">
-                {testSeriesData.map((test, index) => (
-                  <tr className="text-center" key={index}>
-                    <td className="px-4 py-2">{index + 1}</td>
-                    <td className="px-4 py-2 text-blue-600 hover:underline">
-                      <Link to={`/test-reports/${index + 1}`}>
-                        {test.testSeriesName}
-                      </Link>
+                {testSeriesReport.length != 0 ? (
+                  testSeriesReport.map((test, index) => (
+                    <tr className="text-center border-b" key={index}>
+                      <td className="px-4 py-2">{index + 1}</td>
+                      <td
+                        className="px-4 py-2 text-blue-600 hover:text-blue-700 hover:underline cursor-pointer"
+                        onClick={() =>
+                          navigate(`/test-reports/${test.test_series_id}`)
+                        }
+                      >
+                        {test.test_series_id ? test.test_series_id : "NA"}
+                      </td>
+                      <td
+                        className="px-4 py-2 text-blue-600 hover:text-blue-700 hover:underline cursor-pointer"
+                        onClick={() =>
+                          navigate(`/test-reports/${test.test_series_id}`)
+                        }
+                      >
+                        {test.title ? test.title : "Untitled"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.students_joined ? test.students_joined : 0}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr className="text-center">
+                    <td colSpan={4} className="px-4 py-2">
+                      {loading ? (
+                        <>
+                          <span>Loading Test Reports</span>
+                          {"  "}
+                          <BiLoaderAlt
+                            className="inline animate-spin"
+                            size={20}
+                          />
+                        </>
+                      ) : (
+                        "No test series found"
+                      )}
                     </td>
-                    <td className="px-4 py-2">{test.studentsJoined}</td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
           </div>
--- src/screens/TestReportsDetails.jsx
+++ src/screens/TestReportsDetails.jsx
@@ -1,35 +1,33 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../hooks";
+import { server } from "../api";
 import { IoChevronBackOutline } from "react-icons/io5";
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { BiLoaderAlt } from "react-icons/bi";
 
 const TestReportsDetails = () => {
   const { user } = useAuth();
   const { seriesid } = useParams();
   const navigate = useNavigate();
 
-  const testsData = [
-    { name: "Dummy Test 1", attempted: 32, avgMarks: 75, avgTime: 45 },
-    { name: "Dummy Test 2", attempted: 25, avgMarks: 80, avgTime: 50 },
-    { name: "Dummy Test 3", attempted: 40, avgMarks: 85, avgTime: 55 },
-    { name: "Dummy Test 4", attempted: 28, avgMarks: 70, avgTime: 40 },
-    { name: "Dummy Test 5", attempted: 35, avgMarks: 90, avgTime: 60 },
-    { name: "Dummy Test 6", attempted: 30, avgMarks: 78, avgTime: 48 },
-    { name: "Dummy Test 7", attempted: 38, avgMarks: 82, avgTime: 52 },
-    { name: "Dummy Test 8", attempted: 27, avgMarks: 73, avgTime: 42 },
-    { name: "Dummy Test 9", attempted: 33, avgMarks: 88, avgTime: 58 },
-    { name: "Dummy Test 10", attempted: 26, avgMarks: 76, avgTime: 46 },
-    { name: "Dummy Test 11", attempted: 42, avgMarks: 92, avgTime: 62 },
-    { name: "Dummy Test 12", attempted: 29, avgMarks: 79, avgTime: 49 },
-    { name: "Dummy Test 13", attempted: 36, avgMarks: 87, avgTime: 57 },
-    { name: "Dummy Test 14", attempted: 31, avgMarks: 81, avgTime: 53 },
-  ];
+  const [testReport, setTestReport] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // If user is not logged in, redirect to login page
     if (!user) return navigate("/login");
-  }, [user, navigate]);
+
+    server
+      .get(`/api/v1/studio/academy/test-report/${seriesid}`)
+      .then((res) => {
+        setTestReport(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, [user, navigate, seriesid]);
 
   return (
     <section className="md:p-4 lg:p-8">
@@ -47,11 +45,12 @@ const TestReportsDetails = () => {
             <span>Test Reports</span>
           </h2>
           <hr className="my-4" />
-          <div className="min-h-96 h-full overflow-auto">
+          <div className="h-[calc(100dvh-10rem-0.6rem)] lg:h-[calc(100dvh-14rem-0.6rem)] overflow-auto">
             <table className="w-full">
-              <thead className="sticky top-0 bg-white">
+              <thead className="sticky top-0 bg-white border-b">
                 <tr className="text-center">
                   <th className="px-4 py-2">#</th>
+                  <th className="px-4 py-2">Test ID</th>
                   <th className="px-4 py-2">Test Name</th>
                   <th className="px-4 py-2">Attempted</th>
                   <th className="px-4 py-2">Avg Marks</th>
@@ -59,15 +58,45 @@ const TestReportsDetails = () => {
                 </tr>
               </thead>
               <tbody className="text-gray-700 hover:[&>*]:text-gray-950 hover:[&>*]:bg-gray-100">
-                {testsData.map((test, index) => (
-                  <tr className="text-center" key={index}>
-                    <td className="px-4 py-2">{index + 1}</td>
-                    <td className="px-4 py-2">{test.name}</td>
-                    <td className="px-4 py-2">{test.attempted}</td>
-                    <td className="px-4 py-2">{test.avgMarks}</td>
-                    <td className="px-4 py-2">{test.avgTime} mins</td>
+                {testReport.length != 0 ? (
+                  testReport.map((test, index) => (
+                    <tr className="text-center border-b" key={index}>
+                      <td className="px-4 py-2">{index + 1}</td>
+                      <td className="px-4 py-2">
+                        {test.test_id ? test.test_id : "NA"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.title ? test.title : "Untitled"}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.total_attempts ? test.total_attempts : 0}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.obtained_marks ? test.obtained_marks : 0}
+                      </td>
+                      <td className="px-4 py-2">
+                        {test.time_spent ? test.time_spent : 0} mins
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr className="text-center">
+                    <td colSpan={6} className="px-4 py-2">
+                      {loading ? (
+                        <>
+                          <span>Loading Test Reports</span>
+                          {"  "}
+                          <BiLoaderAlt
+                            className="inline animate-spin"
+                            size={20}
+                          />
+                        </>
+                      ) : (
+                        "No Tests found"
+                      )}
+                    </td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
           </div>
