9ada1a4b9e641009758ad9887e327374815f0e19
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2024-11-15 23:52:15 +0530

    fixed signup screen better with full width

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useAuth } from "../hooks";
 import { BiLoaderAlt } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { BsBuildings } from "react-icons/bs";
+import { PiUserCircle } from "react-icons/pi";
 
 const Signup = () => {
   const [data, setData] = useState({
@@ -18,7 +20,6 @@ const Signup = () => {
     password: "",
     confirm_password: "",
   });
-  const [step, setStep] = useState(1);
   const [loading, setLoading] = useState(false);
   const { signup } = useAuth();
 
@@ -51,163 +52,167 @@ const Signup = () => {
   };
 
   return (
-    <div className="bg-[url('/static/images/login-bg.png')] bg-cover bg-center flex h-[calc(100dvh-4rem)]">
-      <div className="w-0 lg:w-1/2"></div>
-      <form
-        className="flex flex-col items-center justify-center w-full lg:w-1/2 bg-white md:bg-opacity-10 backdrop-filter backdrop-blur-lg"
-        onSubmit={handleSignup}
-      >
-        {step === 1 && (
-          <div className="bg-white p-8 rounded-md flex flex-col gap-2 w-full md:w-96">
-            <h1 className="text-2xl font-bold text-center">Welcome Aboard!</h1>
-            <p className="text-center mb-8">
-              The teacher dashboard helps you create and list test series.
-            </p>
-            <input
-              type="text"
-              placeholder="Academy Name"
-              value={data?.academy_name}
-              onChange={(e) =>
-                setData({ ...data, academy_name: e.target.value })
-              }
-              className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
-              required
-            />
-
-            <input
-              type="email"
-              placeholder="Academy Contact Email"
-              value={data?.contact_email}
-              onChange={(e) =>
-                setData({ ...data, contact_email: e.target.value })
-              }
-              className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
-              required
-            />
-
-            <input
-              type="text"
-              placeholder="Academy Contact Phone"
-              value={data?.contact_phone}
-              onChange={(e) =>
-                setData({ ...data, contact_phone: e.target.value })
-              }
-              className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
-              required
-            />
-
-            <button
-              type="button"
-              onClick={() => setStep(2)}
-              className="bg-[#6d45a4] text-white rounded-md flex justify-center gap-2 px-4 py-2"
-            >
-              <BiLoaderAlt
-                className={loading ? "inline animate-spin mt-0.5 " : "hidden"}
-                size={20}
-              />
-              Next: Personal Info
-            </button>
-            <span className="text-center">
-              Already have an account?
-              <br />
-              <Link
-                to="/login"
-                className="text-[#6d45a4]/95 hover:text-[#6d45a4] font-medium"
-              >
-                Login as an Academy
-              </Link>
-            </span>
-          </div>
-        )}
-
-        {step === 2 && (
-          <div className="bg-white p-8 rounded-md flex flex-col gap-2 w-full md:w-96">
-            <h1 className="text-2xl font-bold text-center">
-              Academy Admin Details
-            </h1>
-            <p className="text-center mb-4">
-              This will be your login details for this dashboard.
-            </p>
-            <div className="flex gap-2">
-              <input
-                type="text"
-                placeholder="First Name"
-                value={data?.first_name}
-                onChange={(e) =>
-                  setData({ ...data, first_name: e.target.value })
-                }
-                className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1 w-1/2"
-                required
-              />
-              <input
-                type="text"
-                placeholder="Last Name"
-                value={data?.last_name}
-                onChange={(e) =>
-                  setData({ ...data, last_name: e.target.value })
-                }
-                className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1 w-1/2"
-                required
-              />
+    <>
+      <section className="bg-[url('/static/images/login-bg.png')] bg-cover bg-center md:p-4 lg:p-8 h-[calc(100dvh-4rem)]">
+        <div className="flex flex-col md:flex-row md:gap-4 lg:gap-8">
+          <form
+            className="bg-white md:rounded-md p-4 flex flex-col flex-1"
+            onSubmit={handleSignup}
+          >
+            <div>
+              <h2 className="h-10 text-2xl text-center">
+                <span>Academy Signup</span>
+              </h2>
             </div>
-            <input
-              type="email"
-              placeholder="Email"
-              value={data?.email}
-              onChange={(e) => setData({ ...data, email: e.target.value })}
-              className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={data?.password}
-              onChange={(e) => setData({ ...data, password: e.target.value })}
-              className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={data?.confirm_password}
-              onChange={(e) =>
-                setData({ ...data, confirm_password: e.target.value })
-              }
-              className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2"
-              required
-            />
+            <div className="flex w-full">
+              <div className="flex-1">
+                <div className="h-[calc(100dvh-10rem-0.6rem)] lg:h-[calc(100dvh-14rem-0.6rem)] overflow-auto text-gray-700">
+                  <h2 className="text-xl flex items-center gap-2">
+                    <i className="p-2">
+                      <BsBuildings size={20} />
+                    </i>
+                    <span>Academy Details</span>
+                  </h2>
+                  <span>These information will be public</span>
+                  <hr className="my-4" />
 
-            <button
-              type="submit"
-              className="bg-[#6d45a4] text-white rounded-md flex justify-center gap-2 px-4 py-2"
-            >
-              <BiLoaderAlt
-                className={loading ? "inline animate-spin mt-0.5 " : "hidden"}
-                size={20}
-              />
-              Start Creating
-            </button>
-            <button
-              type="submit"
-              onClick={() => setStep(1)}
-              className="text-[#6d45a4] border border-[#6d45a4] bg-gray-100 hover:bg-gray-200 rounded-md flex justify-center gap-2 px-4 py-2"
-            >
-              Previous Page
-            </button>
-            <span className="text-center">
-              Already have an account?
-              <br />
-              <Link
-                to="/login"
-                className="text-[#6d45a4]/95 hover:text-[#6d45a4] font-medium"
-              >
-                Login as an Academy
-              </Link>
-            </span>
-          </div>
-        )}
-      </form>
-    </div>
+                  <div className="p-8 flex flex-col gap-2 w-full md:w-96 mx-auto">
+                    <label htmlFor="academy_name">Academy Name</label>
+                    <input
+                      type="text"
+                      placeholder="Academy Name"
+                      id="academy_name"
+                      value={data?.academy_name}
+                      onChange={(e) =>
+                        setData({ ...data, academy_name: e.target.value })
+                      }
+                      className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
+                      required
+                    />
+
+                    <label htmlFor="contact_email">Academy Contact Email</label>
+                    <input
+                      type="email"
+                      placeholder="Academy Contact Email"
+                      id="contact_email"
+                      value={data?.contact_email}
+                      onChange={(e) =>
+                        setData({ ...data, contact_email: e.target.value })
+                      }
+                      className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
+                      required
+                    />
+
+                    <label htmlFor="contact_phone">Academy Contact Phone</label>
+                    <input
+                      type="text"
+                      placeholder="Academy Contact Phone"
+                      id="contact_phone"
+                      value={data?.contact_phone}
+                      onChange={(e) =>
+                        setData({ ...data, contact_phone: e.target.value })
+                      }
+                      className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex-1">
+                <h2 className="text-xl flex items-center gap-2">
+                  <i className="p-2">
+                    <PiUserCircle size={20} />
+                  </i>
+                  <span>Academy Admin Details</span>
+                </h2>
+                <span>This will be your login details for this dashboard.</span>
+                <hr className="my-4" />
+
+                <div className="p-8 flex flex-col gap-2 w-full md:w-96 mx-auto">
+                  <div className="flex gap-2">
+                    <input
+                      type="text"
+                      placeholder="First Name"
+                      value={data?.first_name}
+                      onChange={(e) =>
+                        setData({ ...data, first_name: e.target.value })
+                      }
+                      className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1 w-1/2"
+                      required
+                    />
+                    <input
+                      type="text"
+                      placeholder="Last Name"
+                      value={data?.last_name}
+                      onChange={(e) =>
+                        setData({ ...data, last_name: e.target.value })
+                      }
+                      className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1 w-1/2"
+                      required
+                    />
+                  </div>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    value={data?.email}
+                    onChange={(e) =>
+                      setData({ ...data, email: e.target.value })
+                    }
+                    className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2 outline-1"
+                    required
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={data?.password}
+                    onChange={(e) =>
+                      setData({ ...data, password: e.target.value })
+                    }
+                    className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2"
+                    required
+                  />
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={data?.confirm_password}
+                    onChange={(e) =>
+                      setData({ ...data, confirm_password: e.target.value })
+                    }
+                    className="border border-gray-200 bg-gray-100 active:bg-gray-200 rounded-md px-4 py-2 mb-2"
+                    required
+                  />
+                </div>
+                <div className="p-4 flex gap-4 justify-end">
+                  <span className="text-center block text-xs">
+                    Already have an account?
+                    <br />
+                    <Link
+                      to="/login"
+                      className="text-[#6d45a4]/95 hover:text-[#6d45a4] font-medium"
+                    >
+                      Login as an Academy
+                    </Link>
+                  </span>
+
+                  <button
+                    type="submit"
+                    className="bg-[#6d45a4] text-white rounded-md flex justify-center gap-2 px-4 py-2"
+                  >
+                    <BiLoaderAlt
+                      className={
+                        loading ? "inline animate-spin mt-0.5 " : "hidden"
+                      }
+                      size={20}
+                    />
+                    Start Creating
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 
