56c36901250276cd4e048b8f66f11e5bcc4d2aec
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2024-12-23 23:36:39 +0530

    upgraded to yarn 4 and tailwind typescript config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -16,4 +16,4 @@ export default {
     },
   },
   plugins: [require("@tailwindcss/typography")],
-};
+} satisfies Config;
