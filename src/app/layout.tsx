27a5bff98888c4aa99dfd7921718cde225a7da1a
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-02-05 23:57:39 +0530

    fixed build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 const fontInter = Inter({
   variable: "--font-inter",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <div>
-      <h1>Teacher's App|Testkart</h1>
+      <h1>Teacher{`'`}s App|Testkart</h1>
     </div>
   );
 };
