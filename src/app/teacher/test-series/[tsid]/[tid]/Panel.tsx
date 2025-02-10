791c270def41a57a97879587dfc8a50341b510b7
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-02-10 23:52:24 +0530

    fetch and display questions based on selected question

--- src/app/teacher/test-series/[tsid]/[tid]/Panel.tsx
+++ src/app/teacher/test-series/[tsid]/[tid]/Panel.tsx
@@ -62,10 +62,45 @@ interface ApiResponse {
   };
 }
 
+// res 2
+interface Option {
+  option: string;
+  is_correct: boolean;
+  optionId: number;
+}
+
+interface Question2 {
+  question_id: number;
+  test_id: number;
+  subject_id: number;
+  question_type: string;
+  question: string;
+  options: Option[];
+  solution: string;
+  difficulty_level: number;
+  topic: number;
+  sub_topic: number;
+  status: number;
+  index: number;
+  positive_marks: string;
+  negative_marks: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface ApiResponse2 {
+  success: boolean;
+  message: string;
+  data: {
+    question: Question2;
+  };
+}
+
 const Panel = ({ test }: { test: Test }) => {
   const subjects = test.subjects.filter((s) => s.inclued);
   const [subject, setSubject] = useState<number>(subjects[0]?.subject_id);
   const [questions, setQuestions] = useState<Question[]>([]);
+  const [question, setQuestion] = useState<Question2 | null>(null);
   const [questionIndex, setQuestionIndex] = useState<number>(0);
 
   const getQuestions = async () => {
@@ -79,6 +114,23 @@ const Panel = ({ test }: { test: Test }) => {
     );
 
     setQuestions(questions?.data?.questions);
+  };
+
+  const getQuestion = async (index: number) => {
+    const { data: singleQuestion }: { data: ApiResponse2 } = await api.get(
+      `/api/v1/test-series/test/question/${index}`,
+      {
+        headers: {
+          Authorization: `Bearer ${getTokenClient()}`,
+        },
+      }
+    );
+
+    // copy the question to question state
+    let questiontemp = singleQuestion?.data?.question;
+
+    setQuestion(singleQuestion?.data?.question);
+    console.log("QP", singleQuestion);
   };
 
   useEffect(() => {
@@ -118,7 +170,17 @@ const Panel = ({ test }: { test: Test }) => {
                 variant={i === questionIndex ? "default" : "secondary"}
                 type="button"
                 className="w-full rounded-none"
-                onClick={() => setQuestionIndex(i)}
+                onClick={() => {
+                  setQuestionIndex(i);
+                  // check if the question index is present in questions
+                  const existingQuestion = questions.find((q) => q.index === i);
+                  if (existingQuestion) {
+                    console.log("Existing Question", existingQuestion);
+                    getQuestion(existingQuestion?.question_id);
+                  } else {
+                    setQuestion(null);
+                  }
+                }}
                 asChild
               >
                 <div className="justify-between">
@@ -134,8 +196,56 @@ const Panel = ({ test }: { test: Test }) => {
         </div>
       </div>
 
-      <div className="p-2">
-        <div>Questions</div>
+      <div className="p-2 space-y-2 w-full">
+        <div className="flex items-center gap-2">
+          Type:{" "}
+          <Select name="question_type" defaultValue={"MCQ-S"} disabled>
+            <SelectTrigger className="w-48">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value={"MCQ-S"}>MCQ-S</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <hr />
+
+        <div>
+          {/* show question if the question index is present in questions */}
+          {questions
+            .filter((q) => q.index === questionIndex)
+            .map((q) => (
+              <div key={q.question_id}>
+                <div>Question: {q.index + 1}</div>
+                <div>Question ID: {q.question_id}</div>
+              </div>
+            ))}
+
+          {/* if question is null show question not found, else show the questions*/}
+          {question ? (
+            <>
+              <div>Question: {question?.question}</div>
+              <div>Question ID: {question?.question_id}</div>
+              <div>Question Type: {question?.question_type}</div>
+              <div>Difficulty Level: {question?.difficulty_level}</div>
+              <div>Positive Marks: {question?.positive_marks}</div>
+              <div>Negative Marks: {question?.negative_marks}</div>
+              <div>Options:</div>
+              <div>
+                {/* {question?.options?.map((option) => (
+                  <div key={option?.optionId}>
+                    {option?.option} -{" "}
+                    {option?.is_correct ? "Correct" : "Wrong"}
+                  </div>
+                ))} */}
+              </div>
+              <div>Solution: {question?.solution}</div>
+            </>
+          ) : (
+            <div>Question not found</div>
+          )}
+        </div>
       </div>
     </div>
   );
